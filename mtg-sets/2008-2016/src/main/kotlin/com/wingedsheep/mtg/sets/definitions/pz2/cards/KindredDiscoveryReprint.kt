317372cd8f71e37edd73963309f5a6b6f2d3762a
Noah <noah@Mac.localdomain>
2026-09-20 12:50:10 -0700

    Add Kindred Discovery

---
+++ mtg-sets/2008-2016/src/main/kotlin/com/wingedsheep/mtg/sets/definitions/pz2/cards/KindredDiscoveryReprint.kt
@@ -0,0 +1,16 @@
+package com.wingedsheep.mtg.sets.definitions.pz2.cards
+
+import com.wingedsheep.sdk.model.Printing
+import com.wingedsheep.sdk.model.Rarity
+
+val KindredDiscoveryReprint = Printing(
+    oracleId = "005ee549-1bf5-478f-bc3f-3e791bd7eecf",
+    name = "Kindred Discovery",
+    setCode = "PZ2",
+    collectorNumber = "65725",
+    scryfallId = "e0d80c4e-ab81-4aa4-9b89-791675afd615",
+    artist = "Lake Hurwitz",
+    imageUri = "https://cards.scryfall.io/normal/front/e/0/e0d80c4e-ab81-4aa4-9b89-791675afd615.jpg?1783935577",
+    releaseDate = "2017-11-15",
+    rarity = Rarity.RARE,
+)
---
+++ mtg-sets/2026/src/main/kotlin/com/wingedsheep/mtg/sets/definitions/msc/cards/KindredDiscoveryReprint.kt
@@ -0,0 +1,28 @@
+package com.wingedsheep.mtg.sets.definitions.msc.cards
+
+import com.wingedsheep.sdk.model.Printing
+import com.wingedsheep.sdk.model.Rarity
+
+val KindredDiscoveryReprint150 = Printing(
+    oracleId = "005ee549-1bf5-478f-bc3f-3e791bd7eecf",
+    name = "Kindred Discovery",
+    setCode = "MSC",
+    collectorNumber = "150",
+    scryfallId = "19bdedb2-768b-4adf-8193-917a10c8f729",
+    artist = "Stephanie Hans",
+    imageUri = "https://cards.scryfall.io/normal/front/1/9/19bdedb2-768b-4adf-8193-917a10c8f729.jpg?1783903239",
+    releaseDate = "2026-06-26",
+    rarity = Rarity.RARE,
+)
+
+val KindredDiscoveryReprint334 = Printing(
+    oracleId = "005ee549-1bf5-478f-bc3f-3e791bd7eecf",
+    name = "Kindred Discovery",
+    setCode = "MSC",
+    collectorNumber = "334",
+    scryfallId = "7f575fca-cd5d-4f22-a77e-5d9fe380d0d3",
+    artist = "Stephanie Hans",
+    imageUri = "https://cards.scryfall.io/normal/front/7/f/7f575fca-cd5d-4f22-a77e-5d9fe380d0d3.jpg?1783903168",
+    releaseDate = "2026-06-26",
+    rarity = Rarity.RARE,
+)
