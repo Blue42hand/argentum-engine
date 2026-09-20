e4cced8a76588c35a29e40a5fdd2228790f3bc30
Noah <noah@Mac.localdomain>
2026-09-20 12:50:10 -0700

    Add Herald's Horn

---
+++ mtg-sets/2017-2022/src/main/kotlin/com/wingedsheep/mtg/sets/definitions/clb/cards/HeraldsHornReprint.kt
@@ -0,0 +1,16 @@
+package com.wingedsheep.mtg.sets.definitions.clb.cards
+
+import com.wingedsheep.sdk.model.Printing
+import com.wingedsheep.sdk.model.Rarity
+
+val HeraldsHornReprint = Printing(
+    oracleId = "c02c5547-b9c9-4b2d-9d12-e87bfba8f2d2",
+    name = "Herald's Horn",
+    setCode = "CLB",
+    collectorNumber = "862",
+    scryfallId = "178274fd-8b9b-4d69-a47e-31b29b776742",
+    artist = "Jason Felix",
+    imageUri = "https://cards.scryfall.io/normal/front/1/7/178274fd-8b9b-4d69-a47e-31b29b776742.jpg?1783922390",
+    releaseDate = "2022-06-10",
+    rarity = Rarity.UNCOMMON,
+)
