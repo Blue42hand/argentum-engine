b00bb59f22f3ae60e062ded8966d078441131d04
Noah <noah@Mac.localdomain>
2026-09-20 13:14:49 -0700

    Add Sign in Blood to Magic 2010

---
+++ mtg-sets/2026/src/main/kotlin/com/wingedsheep/mtg/sets/definitions/sos/cards/SignInBloodReprint.kt
@@ -0,0 +1,31 @@
+package com.wingedsheep.mtg.sets.definitions.sos.cards
+
+import com.wingedsheep.sdk.model.Printing
+import com.wingedsheep.sdk.model.Rarity
+
+/**
+ * Sign in Blood reprint in SOS. Canonical CardDefinition lives in its earliest set.
+ */
+val SignInBloodReprint99 = Printing(
+    oracleId = "44443716-8356-40cd-a879-35264b29108c",
+    name = "Sign in Blood",
+    setCode = "SOS",
+    collectorNumber = "99",
+    scryfallId = "fe85a124-0d8b-4a29-8df1-65888a39147f",
+    artist = "Anna Steinbauer",
+    imageUri = "https://cards.scryfall.io/normal/front/f/e/fe85a124-0d8b-4a29-8df1-65888a39147f.jpg?1783903675",
+    releaseDate = "2026-04-24",
+    rarity = Rarity.RARE,
+)
+
+val SignInBloodReprint329 = Printing(
+    oracleId = "44443716-8356-40cd-a879-35264b29108c",
+    name = "Sign in Blood",
+    setCode = "SOS",
+    collectorNumber = "329",
+    scryfallId = "1c4c2765-2fb0-43f2-9e50-934405d108d2",
+    artist = "Anna Steinbauer",
+    imageUri = "https://cards.scryfall.io/normal/front/1/c/1c4c2765-2fb0-43f2-9e50-934405d108d2.jpg?1783903594",
+    releaseDate = "2026-04-24",
+    rarity = Rarity.RARE,
+)
