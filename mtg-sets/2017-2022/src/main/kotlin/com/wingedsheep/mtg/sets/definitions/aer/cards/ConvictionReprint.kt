b8768319958666d6e461a209e8d8c9c8e58dfed0
Noah <noah@Mac.localdomain>
2026-09-20 08:03:28 -0700

    Add Conviction and Aether Revolt reprint

---
+++ mtg-sets/2017-2022/src/main/kotlin/com/wingedsheep/mtg/sets/definitions/aer/cards/ConvictionReprint.kt
@@ -0,0 +1,19 @@
+package com.wingedsheep.mtg.sets.definitions.aer.cards
+
+import com.wingedsheep.sdk.model.Printing
+import com.wingedsheep.sdk.model.Rarity
+
+/**
+ * Conviction reprint in Aether Revolt. Canonical CardDefinition lives in Stronghold.
+ */
+val ConvictionReprint = Printing(
+    oracleId = "4374b6b1-e691-4823-802d-8de8530aa461",
+    name = "Conviction",
+    setCode = "AER",
+    collectorNumber = "12",
+    scryfallId = "4f805e97-c28b-4780-b204-74514c0c47d2",
+    artist = "John Stanko",
+    imageUri = "https://cards.scryfall.io/normal/front/4/f/4f805e97-c28b-4780-b204-74514c0c47d2.jpg?1783936781",
+    releaseDate = "2017-01-20",
+    rarity = Rarity.COMMON,
+)
