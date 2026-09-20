93ba07d893e892adacc31d6754657a55310537e7
Noah <noah@Mac.localdomain>
2026-09-20 16:27:05 -0700

    Add Laelia, the Blade Reforged to Commander 2021

---
+++ mtg-sets/2017-2022/src/main/kotlin/com/wingedsheep/mtg/sets/definitions/clb/cards/LaeliaTheBladeReforgedReprint.kt
@@ -0,0 +1,18 @@
+package com.wingedsheep.mtg.sets.definitions.clb.cards
+
+import com.wingedsheep.sdk.model.Printing
+import com.wingedsheep.sdk.model.Rarity
+
+/** Laelia, the Blade Reforged reprint in Commander Legends: Battle for Baldur's Gate. */
+val LaeliaTheBladeReforgedReprint = Printing(
+    oracleId = "a0be9bb2-3234-4c6c-b8ce-0879b1f43003",
+    name = "Laelia, the Blade Reforged",
+    setCode = "CLB",
+    collectorNumber = "801",
+    scryfallId = "2e90b9b2-8e8e-4b00-b3a0-b26dd4551550",
+    artist = "Wisnu Tan",
+    imageUri = "https://cards.scryfall.io/normal/front/2/e/" +
+        "2e90b9b2-8e8e-4b00-b3a0-b26dd4551550.jpg?1783922420",
+    releaseDate = "2022-06-10",
+    rarity = Rarity.RARE,
+)
---
+++ mtg-sets/2024/src/main/kotlin/com/wingedsheep/mtg/sets/definitions/mh3/cards/LaeliaTheBladeReforgedReprints.kt
@@ -0,0 +1,44 @@
+package com.wingedsheep.mtg.sets.definitions.mh3.cards
+
+import com.wingedsheep.sdk.model.Printing
+import com.wingedsheep.sdk.model.Rarity
+
+/** The three Laelia, the Blade Reforged treatments in Modern Horizons 3. */
+val LaeliaTheBladeReforgedReprint281 = Printing(
+    oracleId = "a0be9bb2-3234-4c6c-b8ce-0879b1f43003",
+    name = "Laelia, the Blade Reforged",
+    setCode = "MH3",
+    collectorNumber = "281",
+    scryfallId = "afeb2fe4-f0ef-4366-a568-c36b538f0196",
+    artist = "Wisnu Tan",
+    imageUri = "https://cards.scryfall.io/normal/front/a/f/" +
+        "afeb2fe4-f0ef-4366-a568-c36b538f0196.jpg?1783911213",
+    releaseDate = "2024-06-14",
+    rarity = Rarity.RARE,
+)
+
+val LaeliaTheBladeReforgedReprint368 = Printing(
+    oracleId = "a0be9bb2-3234-4c6c-b8ce-0879b1f43003",
+    name = "Laelia, the Blade Reforged",
+    setCode = "MH3",
+    collectorNumber = "368",
+    scryfallId = "0ac72836-a656-4bdb-adc8-04cf20b77aba",
+    artist = "Tyler Walpole",
+    imageUri = "https://cards.scryfall.io/normal/front/0/a/" +
+        "0ac72836-a656-4bdb-adc8-04cf20b77aba.jpg?1783911185",
+    releaseDate = "2024-06-14",
+    rarity = Rarity.RARE,
+)
+
+val LaeliaTheBladeReforgedReprint482 = Printing(
+    oracleId = "a0be9bb2-3234-4c6c-b8ce-0879b1f43003",
+    name = "Laelia, the Blade Reforged",
+    setCode = "MH3",
+    collectorNumber = "482",
+    scryfallId = "4616106e-b75e-4de4-860e-15369e4abf84",
+    artist = "Wisnu Tan",
+    imageUri = "https://cards.scryfall.io/normal/front/4/6/" +
+        "4616106e-b75e-4de4-860e-15369e4abf84.jpg?1783911147",
+    releaseDate = "2024-06-14",
+    rarity = Rarity.RARE,
+)
