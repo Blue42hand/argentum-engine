5b1ee78c87f723c06c5772de9bf6e467c3ff17ab
Noah <noah@Mac.localdomain>
2026-09-20 00:42:49 -0700

    Add Goblin Chieftain

---
+++ mtg-sets/2008-2016/src/main/kotlin/com/wingedsheep/mtg/sets/definitions/m11/cards/GoblinChieftain.kt
@@ -0,0 +1,15 @@
+package com.wingedsheep.mtg.sets.definitions.m11.cards
+
+import com.wingedsheep.sdk.model.Printing
+import com.wingedsheep.sdk.model.Rarity
+
+val GoblinChieftainReprint = Printing(
+    oracleId = "368b4052-174e-4458-a6e6-eaf8093aa0fe",
+    name = "Goblin Chieftain",
+    setCode = "M11",
+    collectorNumber = "141",
+    artist = "Sam Wood",
+    imageUri = "https://cards.scryfall.io/normal/front/7/8/78563012-b724-4b6e-94e9-8504d3f76d59.jpg?1783941806",
+    releaseDate = "2010-07-16",
+    rarity = Rarity.RARE,
+)
---
+++ mtg-sets/2017-2022/src/main/kotlin/com/wingedsheep/mtg/sets/definitions/jmp/cards/GoblinChieftain.kt
@@ -0,0 +1,15 @@
+package com.wingedsheep.mtg.sets.definitions.jmp.cards
+
+import com.wingedsheep.sdk.model.Printing
+import com.wingedsheep.sdk.model.Rarity
+
+val GoblinChieftainReprint = Printing(
+    oracleId = "368b4052-174e-4458-a6e6-eaf8093aa0fe",
+    name = "Goblin Chieftain",
+    setCode = "JMP",
+    collectorNumber = "324",
+    artist = "Sam Wood",
+    imageUri = "https://cards.scryfall.io/normal/front/8/9/89fcc35b-76fa-4408-8620-a1e11b2caf1f.jpg?1783930390",
+    releaseDate = "2020-07-17",
+    rarity = Rarity.RARE,
+)
