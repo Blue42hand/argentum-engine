7c743b1e05347804530ce97a20a3c91943366a1e
Noah <noah@Mac.localdomain>
2026-09-20 08:36:43 -0700

    Add Coastal Piracy

---
+++ mtg-sets/2003-2007/src/main/kotlin/com/wingedsheep/mtg/sets/definitions/8ed/cards/CoastalPiracyReprint.kt
@@ -0,0 +1,17 @@
+package com.wingedsheep.mtg.sets.definitions.`8ed`.cards
+
+import com.wingedsheep.sdk.model.Printing
+import com.wingedsheep.sdk.model.Rarity
+
+/** Coastal Piracy reprint in Eighth Edition. */
+val CoastalPiracyReprint = Printing(
+    oracleId = "8a05ec32-7b0c-4f23-a4f7-413301c2a70a",
+    name = "Coastal Piracy",
+    setCode = "8ED",
+    collectorNumber = "67",
+    scryfallId = "21d65495-4d73-46fa-bd38-155f4639649a",
+    artist = "Matthew D. Wilson",
+    imageUri = "https://cards.scryfall.io/normal/front/2/1/21d65495-4d73-46fa-bd38-155f4639649a.jpg?1783944810",
+    releaseDate = "2003-07-28",
+    rarity = Rarity.RARE,
+)
