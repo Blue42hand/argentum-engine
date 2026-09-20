5c7547e078b18f7b3c9046281742fb8a51bd3583
Noah <noah@Mac.localdomain>
2026-09-20 11:11:19 -0700

    Add Avenger of Zendikar

---
+++ mtg-sets/2008-2016/src/main/kotlin/com/wingedsheep/mtg/sets/definitions/ddp/cards/AvengerOfZendikarReprint.kt
@@ -0,0 +1,17 @@
+package com.wingedsheep.mtg.sets.definitions.ddp.cards
+
+import com.wingedsheep.sdk.model.Printing
+import com.wingedsheep.sdk.model.Rarity
+
+/** Avenger of Zendikar reprint in DDP. Canonical CardDefinition lives in WWK. */
+val AvengerOfZendikarReprint = Printing(
+    oracleId = "4ba5b3f6-503b-43e6-b66e-4f8c55cffed7",
+    name = "Avenger of Zendikar",
+    setCode = "DDP",
+    collectorNumber = "1",
+    scryfallId = "6a971200-0d89-45e2-8c1e-fb087642f7dc",
+    artist = "Aleksi Briclot",
+    imageUri = "https://cards.scryfall.io/normal/front/6/a/6a971200-0d89-45e2-8c1e-fb087642f7dc.jpg?1783938265",
+    releaseDate = "2015-08-28",
+    rarity = Rarity.MYTHIC,
+)
---
+++ mtg-sets/2017-2022/src/main/kotlin/com/wingedsheep/mtg/sets/definitions/ncc/cards/AvengerOfZendikarReprint.kt
@@ -0,0 +1,17 @@
+package com.wingedsheep.mtg.sets.definitions.ncc.cards
+
+import com.wingedsheep.sdk.model.Printing
+import com.wingedsheep.sdk.model.Rarity
+
+/** Avenger of Zendikar reprint in NCC. Canonical CardDefinition lives in WWK. */
+val AvengerOfZendikarReprint = Printing(
+    oracleId = "4ba5b3f6-503b-43e6-b66e-4f8c55cffed7",
+    name = "Avenger of Zendikar",
+    setCode = "NCC",
+    collectorNumber = "280",
+    scryfallId = "9685faa0-46cc-4098-9ad7-cffece741baa",
+    artist = "Zoltan Boros & Gabor Szikszai",
+    imageUri = "https://cards.scryfall.io/normal/front/9/6/9685faa0-46cc-4098-9ad7-cffece741baa.jpg?1783923254",
+    releaseDate = "2022-04-29",
+    rarity = Rarity.MYTHIC,
+)
