3d96bc369e86009795206e9420339ccbf383bc35
Blue42hand <flamingheads@gmail.com>
2026-09-22 02:52:18 -0700

    Update snapshot batch controller test for leases

--- gym-server/src/test/kotlin/com/wingedsheep/gym/server/controller/SnapshotBatchControllerTest.kt
+++ gym-server/src/test/kotlin/com/wingedsheep/gym/server/controller/SnapshotBatchControllerTest.kt
@@ -1,13 +1,15 @@
 package com.wingedsheep.gym.server.controller
 
 import com.wingedsheep.engine.registry.CardRegistry
+import com.wingedsheep.gym.server.lifecycle.EnvLeaseManager
 import com.wingedsheep.gym.service.MultiEnvService
 import io.kotest.core.spec.style.FunSpec
 import io.kotest.matchers.shouldBe
 
 class SnapshotBatchControllerTest : FunSpec({
     test("empty snapshot batch maps to an empty response") {
-        val controller = SnapshotBatchController(MultiEnvService(CardRegistry()))
+        val service = MultiEnvService(CardRegistry())
+        val controller = SnapshotBatchController(service, EnvLeaseManager(service, ttlMs = 0))
 
         controller.snapshotBatch(emptyList()) shouldBe emptyList()
     }
