31cd34418fca1b328ff543f5a900c149aa90bdfe
Blue42hand <flamingheads@gmail.com>
2026-09-22 02:51:47 -0700

    Update observe batch controller test for leases

--- gym-server/src/test/kotlin/com/wingedsheep/gym/server/controller/ObserveBatchControllerTest.kt
+++ gym-server/src/test/kotlin/com/wingedsheep/gym/server/controller/ObserveBatchControllerTest.kt
@@ -1,13 +1,15 @@
 package com.wingedsheep.gym.server.controller
 
 import com.wingedsheep.engine.registry.CardRegistry
+import com.wingedsheep.gym.server.lifecycle.EnvLeaseManager
 import com.wingedsheep.gym.service.MultiEnvService
 import io.kotest.core.spec.style.FunSpec
 import io.kotest.matchers.shouldBe
 
 class ObserveBatchControllerTest : FunSpec({
     test("empty observe batch maps to an empty response") {
-        val controller = ObserveBatchController(MultiEnvService(CardRegistry()))
+        val service = MultiEnvService(CardRegistry())
+        val controller = ObserveBatchController(service, EnvLeaseManager(service, ttlMs = 0))
 
         controller.observeBatch(emptyList()) shouldBe emptyList()
     }
