04ffc02f843a709b333d65fa9f4b7e71b68a0f59
Blue42hand <flamingheads@gmail.com>
2026-09-22 02:51:31 -0700

    Update decision batch controller test for leases

--- gym-server/src/test/kotlin/com/wingedsheep/gym/server/controller/DecisionBatchControllerTest.kt
+++ gym-server/src/test/kotlin/com/wingedsheep/gym/server/controller/DecisionBatchControllerTest.kt
@@ -1,13 +1,15 @@
 package com.wingedsheep.gym.server.controller
 
 import com.wingedsheep.engine.registry.CardRegistry
+import com.wingedsheep.gym.server.lifecycle.EnvLeaseManager
 import com.wingedsheep.gym.service.MultiEnvService
 import io.kotest.core.spec.style.FunSpec
 import io.kotest.matchers.shouldBe
 
 class DecisionBatchControllerTest : FunSpec({
     test("empty decision batch maps to an empty response") {
-        val controller = DecisionBatchController(MultiEnvService(CardRegistry()))
+        val service = MultiEnvService(CardRegistry())
+        val controller = DecisionBatchController(service, EnvLeaseManager(service, ttlMs = 0))
 
         controller.submitDecisionBatch(emptyList()) shouldBe emptyList()
     }
