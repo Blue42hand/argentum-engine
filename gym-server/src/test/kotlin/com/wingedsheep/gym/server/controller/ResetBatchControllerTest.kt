2c15c2c3a7ad3d1ac4ee1a127c837683155b0cf2
Blue42hand <flamingheads@gmail.com>
2026-09-22 02:51:56 -0700

    Update reset batch controller test for leases

--- gym-server/src/test/kotlin/com/wingedsheep/gym/server/controller/ResetBatchControllerTest.kt
+++ gym-server/src/test/kotlin/com/wingedsheep/gym/server/controller/ResetBatchControllerTest.kt
@@ -1,13 +1,15 @@
 package com.wingedsheep.gym.server.controller
 
 import com.wingedsheep.engine.registry.CardRegistry
+import com.wingedsheep.gym.server.lifecycle.EnvLeaseManager
 import com.wingedsheep.gym.service.MultiEnvService
 import io.kotest.core.spec.style.FunSpec
 import io.kotest.matchers.shouldBe
 
 class ResetBatchControllerTest : FunSpec({
     test("empty reset batch maps to an empty response") {
-        val controller = ResetBatchController(MultiEnvService(CardRegistry()))
+        val service = MultiEnvService(CardRegistry())
+        val controller = ResetBatchController(service, EnvLeaseManager(service, ttlMs = 0))
 
         controller.resetBatch(emptyList()) shouldBe emptyList()
     }
