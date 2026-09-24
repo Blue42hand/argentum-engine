25a21d3634fdb51a6c6b7de3565593170f37304b
Blue42hand <flamingheads@gmail.com>
2026-09-23 19:15:16 -0700

    Add Arcane Denial reprint to Commander 2016

---
+++ mtg-sets/2008-2016/src/main/kotlin/com/wingedsheep/mtg/sets/definitions/c16/cards/ArcaneDenialReprint.kt
@@ -0,0 +1,19 @@
+package com.wingedsheep.mtg.sets.definitions.c16.cards
+
+import com.wingedsheep.sdk.model.Printing
+import com.wingedsheep.sdk.model.Rarity
+
+/**
+ * Arcane Denial reprint in Commander 2016. Canonical CardDefinition lives in Alliances.
+ */
+val ArcaneDenialReprint = Printing(
+    oracleId = "ab1cc360-b9de-48d9-9983-4dfe4a7d2a37",
+    name = "Arcane Denial",
+    setCode = "C16",
+    collectorNumber = "83",
+    scryfallId = "a4746d2d-7bb8-4f97-b07e-ed0682892d8e",
+    artist = "Mark Zug",
+    imageUri = "https://cards.scryfall.io/normal/front/a/4/a4746d2d-7bb8-4f97-b07e-ed0682892d8e.jpg?1783937074",
+    releaseDate = "2016-11-11",
+    rarity = Rarity.COMMON,
+)
