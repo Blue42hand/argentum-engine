fc5d1304e98ca5d0a98e348d5047454025db1df8
Blue42hand <flamingheads@gmail.com>
2026-09-23 19:16:06 -0700

    Add Arcane Denial reprint to Marvel Super Heroes Commander

---
+++ mtg-sets/2026/src/main/kotlin/com/wingedsheep/mtg/sets/definitions/msc/cards/ArcaneDenialReprint.kt
@@ -0,0 +1,19 @@
+package com.wingedsheep.mtg.sets.definitions.msc.cards
+
+import com.wingedsheep.sdk.model.Printing
+import com.wingedsheep.sdk.model.Rarity
+
+/**
+ * Arcane Denial reprint in Marvel Super Heroes Commander. Canonical CardDefinition lives in Alliances.
+ */
+val ArcaneDenialReprint = Printing(
+    oracleId = "ab1cc360-b9de-48d9-9983-4dfe4a7d2a37",
+    name = "Arcane Denial",
+    setCode = "MSC",
+    collectorNumber = "147",
+    scryfallId = "5eb843ef-b223-40d0-a461-54b16d188d0e",
+    artist = "Bastien L. Deharme",
+    imageUri = "https://cards.scryfall.io/normal/front/5/e/5eb843ef-b223-40d0-a461-54b16d188d0e.jpg?1783903242",
+    releaseDate = "2026-06-26",
+    rarity = Rarity.COMMON,
+)
