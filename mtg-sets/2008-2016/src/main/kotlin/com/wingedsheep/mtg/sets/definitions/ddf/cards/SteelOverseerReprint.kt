3eecadba65140adf6a1c6547b39b9567caa64822
Blue42hand <flamingheads@gmail.com>
2026-09-24 03:40:57 -0700

    cards: register Steel Overseer reprints

---
+++ mtg-sets/2008-2016/src/main/kotlin/com/wingedsheep/mtg/sets/definitions/ddf/cards/SteelOverseerReprint.kt
@@ -0,0 +1,19 @@
+package com.wingedsheep.mtg.sets.definitions.ddf.cards
+
+import com.wingedsheep.sdk.model.Printing
+import com.wingedsheep.sdk.model.Rarity
+
+/**
+ * Steel Overseer reprint in DDF. Canonical CardDefinition lives in M11.
+ */
+val SteelOverseerReprint = Printing(
+    oracleId = "986ae327-f433-4c58-93dc-afc544b9bfcb",
+    name = "Steel Overseer",
+    setCode = "DDF",
+    collectorNumber = "44",
+    scryfallId = "292ac228-7a1e-4c21-b81e-204014c0f3b1",
+    artist = "Chris Rahn",
+    imageUri = "https://cards.scryfall.io/normal/front/2/9/292ac228-7a1e-4c21-b81e-204014c0f3b1.jpg",
+    releaseDate = "2010-09-03",
+    rarity = Rarity.RARE,
+)
