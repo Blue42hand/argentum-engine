429343e35271fe659bf67f3b887c286fa6703ab6
Blue42hand <flamingheads@gmail.com>
2026-09-23 19:15:57 -0700

    Add Arcane Denial reprint to Secrets of Strixhaven Commander

---
+++ mtg-sets/2026/src/main/kotlin/com/wingedsheep/mtg/sets/definitions/soc/cards/ArcaneDenialReprint.kt
@@ -0,0 +1,19 @@
+package com.wingedsheep.mtg.sets.definitions.soc.cards
+
+import com.wingedsheep.sdk.model.Printing
+import com.wingedsheep.sdk.model.Rarity
+
+/**
+ * Arcane Denial reprint in Secrets of Strixhaven Commander. Canonical CardDefinition lives in Alliances.
+ */
+val ArcaneDenialReprint = Printing(
+    oracleId = "ab1cc360-b9de-48d9-9983-4dfe4a7d2a37",
+    name = "Arcane Denial",
+    setCode = "SOC",
+    collectorNumber = "187",
+    scryfallId = "4931aa7c-b77b-4002-bfb1-b75a1bac1deb",
+    artist = "Xi Zhang",
+    imageUri = "https://cards.scryfall.io/normal/front/4/9/4931aa7c-b77b-4002-bfb1-b75a1bac1deb.jpg?1783903794",
+    releaseDate = "2026-04-24",
+    rarity = Rarity.COMMON,
+)
