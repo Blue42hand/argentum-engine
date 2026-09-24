00bbcc2f2b0747db09a6c64c458819f14f621bda
Blue42hand <flamingheads@gmail.com>
2026-09-23 19:15:40 -0700

    Add Arcane Denial reprint to Wilds of Eldraine Commander

---
+++ mtg-sets/2023/src/main/kotlin/com/wingedsheep/mtg/sets/definitions/woc/cards/ArcaneDenialReprint.kt
@@ -0,0 +1,19 @@
+package com.wingedsheep.mtg.sets.definitions.woc.cards
+
+import com.wingedsheep.sdk.model.Printing
+import com.wingedsheep.sdk.model.Rarity
+
+/**
+ * Arcane Denial reprint in Wilds of Eldraine Commander. Canonical CardDefinition lives in Alliances.
+ */
+val ArcaneDenialReprint = Printing(
+    oracleId = "ab1cc360-b9de-48d9-9983-4dfe4a7d2a37",
+    name = "Arcane Denial",
+    setCode = "WOC",
+    collectorNumber = "84",
+    scryfallId = "0adf3b42-f959-4622-b2d5-c70323c30a7f",
+    artist = "Xi Zhang",
+    imageUri = "https://cards.scryfall.io/normal/front/0/a/0adf3b42-f959-4622-b2d5-c70323c30a7f.jpg?1783914964",
+    releaseDate = "2023-09-08",
+    rarity = Rarity.COMMON,
+)
